597cc1d3576b92b5507cd53766c87050d7eb1d70
Enakistehen <44033761+Enakistehen@users.noreply.github.com>
2018-10-15 15:18:20 +0200

    Átírások a loop előttig
    Kb. a void loop() függvény előtti 130 sor megvan, persze még igényel módosításokat.

--- cnc.ino
+++ cnc.ino
@@ -1,32 +1,50 @@
 /* 
- Mini CNC Plotter firmware, based in TinyCNC https://github.com/MakerBlock/TinyCNC-Sketches
- Send GCODE to this Sketch using gctrl.pde https://github.com/damellis/gctrl
- Convert SVG to GCODE with MakerBot Unicorn plugin for Inkscape available here https://github.com/martymcguire/inkscape-unicorn
- 
- More information about the Mini CNC Plotter here (german, sorry): http://www.makerblog.at/2015/02/projekt-mini-cnc-plotter-aus-alten-cddvd-laufwerken/
-  */
+ Elvileg a plotter kódja, A4988 motorvezérlőre írva. 
+ */
 
+#include <Arduino.h>
 #include <Servo.h>
-#include <Stepper.h>
+#include "BasicStepperDriver.h"
+#include "MultiDriver.h"
+#include "SyncDriver.h"
 
 #define LINE_BUFFER_LENGTH 512
+// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+#define MOTOR_STEPS 200
+// Since microstepping is set externally, make sure this matches the selected mode
+// If it doesn't, the motor will move at a different RPM than chosen
+// 1=full step, 2=half step etc.
+#define MICROSTEPS 32
+
+// X motor
+#define DIR_X 8
+#define STEP_X 9
+
+// Y motor
+#define DIR_Y 6
+#define STEP_Y 7
+
+// RPM értékek
+// TODO: Kitalálni, ezek mekkorák legyenek
+#define RPM_X 5
+#define RPM_Y 5
+
+// 2-wire basic config, microstepping is hardwired on the driver
+// Other drivers can be mixed and matched but must be configured individually
+BasicStepperDriver stepperX(MOTOR_STEPS, DIR_X, STEP_X);
+BasicStepperDriver stepperY(MOTOR_STEPS, DIR_Y, STEP_Y);
 
 // Servo position for Up and Down 
 const int penZUp = 80;
 const int penZDown = 40;
 
 // Servo on PWM pin 6
-const int penServoPin = 6;
+// TODO: Ezt is megnézni, hogy jó-e
+const int penServoPin = 6; 
 
-// Should be right for DVD steppers, but is not too important here
-const int stepsPerRevolution = 20; 
-
-// create servo object to control a servo 
+// Ez a sor az eredetiből van, és valszeg fog kelleni, mert ez a szervónak kell, nem a steppernek
+// Egyelőre bennhagyom
 Servo penServo;  
-
-// Initialize steppers for X- and Y-axis using this Arduino pins for the L293D H-bridge
-Stepper myStepperY(stepsPerRevolution, 2,3,4,5);            
-Stepper myStepperX(stepsPerRevolution, 8,9,10,11);  
 
 /* Structures, global variables    */
 struct point { 
@@ -36,6 +54,7 @@ struct point {
 };
 
 // Current position of plothead
+// Ez is az ere
 struct point actuatorPos;
 
 //  Drawing settings, should be OK
@@ -47,21 +66,23 @@ int penDelay = 50;
 // Motor steps to go 1 millimeter.
 // Use test sketch to go 100 steps. Measure the length of line. 
 // Calculate steps per mm. Enter here.
+// TODO: Ezt mérjük le valamikor. Valszeg nekünk is ennyi lesz. 
 float StepsPerMillimeterX = 6.0;
 float StepsPerMillimeterY = 6.0;
 
 // Drawing robot limits, in mm
 // OK to start with. Could go up to 50 mm if calibrated well. 
+// TODO: Ezt is állítsuk be. Nekünk egy jó 150 körüli kéne legyen, szóval egyelőre 100-ra állítom, mert az bőven kevés. 
 float Xmin = 0;
-float Xmax = 40;
+float Xmax = 100;
 float Ymin = 0;
-float Ymax = 40;
+float Ymax = 100;
 float Zmin = 0;
 float Zmax = 1;
 
-float Xpos = Xmin;
-float Ypos = Ymin;
-float Zpos = Zmax; 
+float Xpos = Xmin; // X tengelyen alaphelyzet
+float Ypos = Ymin; // Y tengelyen alaphelyzet
+float Zpos = Zmax; // Z tengelyen alaphelyzet - a toll fel van emelve
 
 // Set to true to get debug output.
 boolean verbose = false;
@@ -81,13 +102,14 @@ void setup() {
   //  Setup
   Serial.begin( 9600 );
   
-  penServo.attach(penServoPin);
+  penServo.attach(penServoPin); // Mondtam, hogy fog kelleni az a Servo ott a 40. sorban
   penServo.write(penZUp);
   delay(200);
 
   // Decrease if necessary
-  myStepperX.setSpeed(250);
-  myStepperY.setSpeed(250);  
+  // TODO: Itt nem vagyok biztos, hogy csak ennyi kell, de egyelőre legyen.  
+  stepperX.begin(RPM_X, MICROSTEPS);
+  stepperY.begin(RPM_Y, MICROSTEPS);  
 
   //  Set & move to initial default position
   // TBD
